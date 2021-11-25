6ff175c623cc6197f4c0ff253de8ac7795a8d75c
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-11-25 23:56:45 +0000

    OK-tier version

--- src/Main.hs
+++ src/Main.hs
@@ -18,13 +18,14 @@ main = scotty 3000 $ do
   get "/img/output.png" $ do
     file "img/output.png"
 
+  get "/img/lambda.jpg" $ do
+    file "img/lambda.jpg"
+
   get "/style/style.css" $ do
     file "style/style.css"
 
-  get "/favicon.ico" $ do
-    file "favicon.ico"
-
--- Taken from https://mmhaskell.com/blog/2020/3/9/blaze-lightweight-html-generation 
+-- Structure taken from lectures
+-- CSS loading from https://mmhaskell.com/blog/2020/3/9/blaze-lightweight-html-generation (better saying where I found it, as I found it quite fun)
 home :: H.Html
 home = do
   H.head $ do
@@ -32,7 +33,17 @@ home = do
     H.link H.! A.rel "stylesheet" H.! A.href "style/style.css"
   H.body $ do
     H.h1 "Welcome to our Haskell-written site!"
-    H.a H.! A.href "/img" $ H.span "Texte"
+    H.img H.! A.src "../img/lambda.jpg" H.! A.alt "Haskell logo"
+    H.p "This is a project written (almost) completely in Haskell (except for a CSS file) : it uses an eDSL for Shapes extended from what we saw during lectures."
+    H.br 
+    H.p "If you found this page, you are at the right place. However, shall you want to look at the code, you can either follow" 
+    H.a H.! A.href "https://github.com/LogarithmeNeper/shape-server" $ H.span "this link"
+    H.p "or look at the code in your favourite IDE (given that you downloaded it, which is the case). Below are links you can follow to see images, and code used to produce them"
+    H.ul $ do
+      H.li $ do H.a H.! A.href "/img" $ H.span "Texte"
+      H.li "Second item"
+      H.li "Third item"
+    
 
 display :: Text
 display = do R.renderHtml $ do myImage
