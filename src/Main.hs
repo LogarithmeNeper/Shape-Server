6255f05a4239946d80f24e696d6f16406436fda5
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-11-25 22:16:48 +0000

    first working version

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,30 @@
 
 module Main where
 
-import Web.Scotty 
-main = scotty 3000 $ do 
-  get "/" $ do 
-    html "Hello World!"
+import Data.Text.Lazy
+import Web.Scotty
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import qualified Text.Blaze.Html.Renderer.Text as R
+
+main = scotty 3000 $ do
+  get "/" $ do
+    html $ link
+  
+  get "/img" $ do
+    html $ display
+
+  get "/img/output.png" $ do
+    file "img/output.png"
+
+link :: Text
+link = do R.renderHtml $ do myList
+
+myList :: H.Html
+myList = H.a H.! A.href "/img" $ H.span "Texte"
+
+display :: Text
+display = do R.renderHtml $ do myImage
+
+myImage :: H.Html 
+myImage = H.img H.! A.src "../img/output.png" H.! A.alt "Contemporary art."
