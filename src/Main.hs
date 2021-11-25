c6e73844852e55b43d24e0149be03c18fdcb13bb
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-11-25 21:21:28 +0000

    basic scotty example

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,8 @@
-module Main where
-
-import Shapes
-import Render (render,defaultWindow)
-import Codec.Picture (PixelRGB8 (PixelRGB8))
-
-exampleDrawing =  [ ((translate (point 0.5 0.25) <+> scale (point 1.2 0.4)), (rectangle 1 0.25, PixelRGB8 45 10 80, 1)), ((scale (point 0.5 0.25) <+> scale (point 1.2 0.4)), (ellipse 1 1, PixelRGB8 90 10 0, 2)) ]
-
-main = render "img/output.png" defaultWindow exampleDrawing
-
-{-
-
 {-# LANGUAGE OverloadedStrings #-}
+
+module Main where
 
 import Web.Scotty 
 main = scotty 3000 $ do 
   get "/" $ do 
     html "Hello World!"
-
--}
